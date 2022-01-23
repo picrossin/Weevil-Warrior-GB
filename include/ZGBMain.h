79b0635e900c00830a5937262f9d77c1f187421f
Jaden Goter <jadengoter@gmail.com>
2022-01-22 20:09:42 -0600

    Added up spikes.

--- include/ZGBMain.h
+++ include/ZGBMain.h
@@ -8,6 +8,7 @@ STATE_DEF_END
 #define SPRITES \
 _SPRITE_DMG(SpritePlayer, player)\
 _SPRITE_DMG(SpriteEnemy, enemy)\
+_SPRITE_DMG(SpriteSpikeUp, spikes)\
 SPRITE_DEF_END
 
 #include "ZGBMain_Init.h"
